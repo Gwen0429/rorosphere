bdb818dbf1c45ce6fc64947fe2baf5a4508960e1
Gwen0429 <goodmanjingwenzhou@icloud.com>
2025-08-05 19:53:26 +0800

    👌

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -25,28 +25,37 @@ export default function NavBar() {
   };
 
   const svgIcon = `
-  <svg width="28" height="28" viewBox="0 0 280 280" xmlns="http://www.w3.org/2000/svg">
-    <defs>
-      <radialGradient id="darkGoldMetalDirect" cx="50%" cy="50%" r="70%">
-        <stop offset="0%" stop-color="#C9B37E" />
-        <stop offset="50%" stop-color="#8B734C" />
-        <stop offset="100%" stop-color="#4A3A1F" />
-      </radialGradient>
-      <filter id="innerGlowDirect" x="-50%" y="-50%" width="200%" height="200%">
-        <feOffset dx="0" dy="0" />
-        <feGaussianBlur stdDeviation="3.5" result="glow" />
-        <feMerge>
-          <feMergeNode in="glow"/>
-          <feMergeNode in="SourceGraphic"/>
-        </feMerge>
-      </filter>
-    </defs>
-    <path
-      d="M140,70 C168,75 170,115 152,135 C165,150 155,180 140,200 C125,180 115,150 128,135 C110,115 112,75 140,70 Z"
-      fill="url(#darkGoldMetalDirect)"
-      filter="url(#innerGlowDirect)"
-    />
-  </svg>
+    <svg width="28" height="28" viewBox="0 0 280 280" xmlns="http://www.w3.org/2000/svg">
+      <defs>
+        <path id="softPetal" d="M0,-70 C28,-65 30,-25 12,-5 C25,10 15,40 0,60 C-15,40 -25,10 -12,-5 C-30,-25 -28,-65 0,-70 Z" />
+        <radialGradient id="darkGoldMetal" cx="50%" cy="50%" r="70%">
+          <stop offset="0%" stop-color="#C9B37E" />
+          <stop offset="50%" stop-color="#8B734C" />
+          <stop offset="100%" stop-color="#4A3A1F" />
+        </radialGradient>
+        <filter id="innerGlow" x="-50%" y="-50%" width="200%" height="200%">
+          <feOffset dx="0" dy="0" />
+          <feGaussianBlur stdDeviation="3.5" result="glow" />
+          <feMerge>
+            <feMergeNode in="glow"/>
+            <feMergeNode in="SourceGraphic"/>
+          </feMerge>
+        </filter>
+        <radialGradient id="coreLight" cx="50%" cy="50%" r="100%">
+          <stop offset="0%" stop-color="#E0C58F" />
+          <stop offset="100%" stop-color="#3E2D12" />
+        </radialGradient>
+      </defs>
+      <g transform="translate(140,140)">
+        <use href="#softPetal" fill="url(#darkGoldMetal)" filter="url(#innerGlow)" transform="rotate(0)" />
+        <use href="#softPetal" fill="url(#darkGoldMetal)" filter="url(#innerGlow)" transform="rotate(60)" />
+        <use href="#softPetal" fill="url(#darkGoldMetal)" filter="url(#innerGlow)" transform="rotate(120)" />
+        <use href="#softPetal" fill="url(#darkGoldMetal)" filter="url(#innerGlow)" transform="rotate(180)" />
+        <use href="#softPetal" fill="url(#darkGoldMetal)" filter="url(#innerGlow)" transform="rotate(240)" />
+        <use href="#softPetal" fill="url(#darkGoldMetal)" filter="url(#innerGlow)" transform="rotate(300)" />
+        <circle cx="0" cy="0" r="18" fill="url(#coreLight)" />
+      </g>
+    </svg>
   `;
 
   return (
@@ -118,6 +127,7 @@ export default function NavBar() {
     </nav>
   );
 }
+
 
 
 
