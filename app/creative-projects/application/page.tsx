4d0243aefd3936046739816e162e67537af2f597
Gwen0429 <goodmanjingwenzhou@icloud.com>
2025-08-10 18:01:22 +0800

    hahaha

--- app/creative-projects/application/page.tsx
+++ app/creative-projects/application/page.tsx
@@ -1,8 +1,12 @@
 'use client';
 
+import React, { Suspense } from 'react';
 import ClientApplication from './ClientApplication';
 
 export default function ApplicationPage() {
-  return <ClientApplication />;
+  return (
+    <Suspense fallback={<div>加载中...</div>}>
+      <ClientApplication />
+    </Suspense>
+  );
 }
-
